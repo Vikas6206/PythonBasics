7f02333e402d81fbbc55ddd25689a42834c8ee48
Vikas6206 <vikas.kr6206@gmail.com>
2019-05-18 14:41:43 +0530

    Added the for loopr file

---
+++ home/sample/beginner/ForLoopExamples.py
@@ -0,0 +1,24 @@
+for item in 'Python':
+    print(item)
+
+
+for item in ['Python','Java','JavaScript']:
+    print(item)
+
+
+for item in range(10):
+    print(item)
+
+for item in range(5,10):
+    print(item)
+
+for item in range(5,10,2):
+    print(item)
+
+
+for x in range(5):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+for x in [5,2,5,2,2]:
+    print('*' * x)
